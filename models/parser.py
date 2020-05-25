103b583b5a98e6605b327a9a202298bcef884944
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2020-05-25 16:06:02 +0200

    training

--- models/parser.py
+++ models/parser.py
@@ -2,42 +2,83 @@
 # coding : utf-8
 
 import unidecode
-
+import re
+from config.constant import STOP_WORDS
 
 class Parser:
     
     def __init__(self, sentence):
         self.sentence = sentence
+        self.special_characters = ",?;.:/!-*+%$€_£¤=@|°}]¨[(){'#~&²"
 
     def get_lower_cases(self):
+        """method to return sentence in lower cases"""
         return self.sentence.lower()
 
-    def remove_spaces(self, sentence):
-        self.sentence = sentence
-        no_space = self.sentence.strip()
-        return no_space
-
     def remove_all_accents(self, sentence):
+        """method to remove all accents with unidecode"""
         # convert my string with unicode to remove accents
         unaccented_string = unidecode.unidecode(sentence)
         self.sentence = unaccented_string
         return self.sentence
 
     def remove_all_special_characters(self, sentence):
-        sentence = ",?;.:/!-*+%$€_£¤=@|°}]¨[(){'#~&²"
+        """method to remove all special characters
+        from the string 'sentence'"""
         remove_spec_characters = "                                "
-        delete = str.maketrans(sentence, remove_spec_characters)
-        self.sentence = self.sentence.translate(delete)
+        to_translate = str.maketrans(self.special_characters, remove_spec_characters)
+        self.sentence = self.sentence.translate(to_translate)
         return self.sentence
+
+    def remove_spaces(self, sentence):
+        """method to remove all spaces beggining and end,
+        and removes repetitive spaces"""
+        self.sentence = sentence.strip().replace("  ", " ").replace("   ", " ").replace("'", "")
+        return self.sentence
+    
+    def remove_stop_words(self, sentence):
+        """method that removes all useless words in sentence,
+        we get in return the main info from the sentence """
+        
+        cleaned_sentence = []
+
+        for word in self.sentence.split(): # maybe add a new list
+            if word in STOP_WORDS:
+                pass
+            if word not in STOP_WORDS:
+                cleaned_sentence.append(word)
+            if word == " ":
+                pass
+            self.cleaned_sentence = " ".join(cleaned_sentence)
+
+        return self.cleaned_sentence
+
+    def capture_regular_expression(self, sentence):
+        """method that captures location in 
+        string, regular expression"""
+
+        sentence = str(sentence)
+
+        regex = r"(l'adresse (de|du)|où est|où se situe|où se trouve|à quel endroit est|indiquer|me montrer) (?P<lieu>[^,.:;!?]+)"
+        match = re.search(regex, sentence)
+        adress = match.group("lieu") #ou group(3)
+        self.adress = adress
+
+        return self.adress
+
+    # commencer par test
+    # create a method using library re
+    # create differents lists : introduction à une question pour un lieu
+    # préparer des réponses positives
+    # préparer des réponses négatives (reformuler la question du utilisateur)
+    # préparer la variable qui retient seulement le lieu en question (group1,2,3 choisir groupe 3)
 
 
 def main():
     pass
-    # parser = Parser("hello")
-    # print(parser.sentence)
 
 if __name__ == "__main__":
-    main()
+    pass
 
 
         
---
+++ test/test_parser.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python3
+# coding : utf-8
+
+from models.parser import Parser
+import pytest
+
+def test_create_parser():
+    p = Parser("")
+    assert p.sentence == ""
+
+def test_get_lower_cases():
+    p = Parser("Hello")
+    assert p.get_lower_cases() == "hello"
+
+def test_remove_spaces():
+    p = Parser("  Hello  ")
+    assert p.remove_spaces("  Hello l'o  ") == "Hello lo"
+
+def test_remove_all_accents():
+    p = Parser("éèêëãàäâåîïìöôòõñûüÿ")
+    assert p.remove_all_accents("éèêëãàäâåîïìöôòõñûüÿ") ==  "eeeeaaaaaiiioooonuuy"
+
+def test_take_away_special_characters():
+    p = Parser("Hello my name is Jon ,?;.:/!-*+%$€_£¤=@|°}]¨[(){'#~&²")
+    assert p.remove_all_special_characters("hello my name is Jon ,?;.:/!-*+%$€_£¤=@|°}]¨[(){'#~&²") \
+    == "Hello my name is Jon                                 "
+
+#test1 - take important words 
+def test_remove_stop_words():
+    sentence = "salut grandpy je souhaite savoir ou se situe la tour montparnasse"
+    p = Parser(sentence)
+    assert p.remove_stop_words(sentence) == "tour montparnasse"
+
+#test2 - take important words 
+def test_remove_stop_words():
+    sentence = "salut grandpy a quel endroit se trouve le musee du louvre"
+    p = Parser(sentence)
+    assert p.remove_stop_words(sentence) == "musee louvre"
+
+# test3 --> keeper
+def test_capture_regular_expression():
+    sentence = "Salut grandpy! Comment s'est passé ta soirée avec Grandma hier soir? Au fait, pendant que j'y pense, pourrais-tu m'indiquer où se trouve le musée d'art et d'histoire de Fribourg, s'il te plaît?"
+    p = Parser(sentence)
+    assert p.capture_regular_expression(sentence) == "où se trouve le musée d'art et d'histoire de Fribourg"
+
