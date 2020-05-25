909d1f4b1d0b93e571cf67318f2825c3f5307a14
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2020-05-25 16:08:12 +0200

    sandbox for regex

---
+++ test/entrainement_re.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python3
+# coding : utf-8
+
+import re
+
+class Regex:
+
+    regex1 = "ou se situe la tour eiffel"
+    regex2 = "a quel endroit est la tour eiffel"
+    regex3 = "ou se trouve la tour eiffel"
+
+    def etape1(self):
+        pass
+
+    def etape2(self):
+        pass
+
+    def etape3(self):
+        pass
+
+
+if __name__ == "__main__":
+    print(re.match(r"SI(.)?UER", "SITUER"))
+
+
+
