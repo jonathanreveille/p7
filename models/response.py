4bab09b375688b36621f8814809c2bba26d3af7f
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2020-05-25 16:08:35 +0200

    response for grandpy script

---
+++ models/response.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python3
+# coding : utf-8
+
+# Those sentences will be used when Grandpy
+# does not understand the question from a user
+
+
+negative_answers = [
+    "Oh bien mon petit, je ne vois plus grand chose...peux-tu me poser la question à nouveau ?",
+    "Tu sais avec le temps, j'ai besoin qu'on me repète la question, peux-tu reformuler s'il te plaît ?",
+    "J'entends plus rien avec l'âge tu sais...Que m'as-tu-dit ?",
+    "Rapproche toi un peu que j'entende mieux s'il te plaît. Peux-tu me poser ta question à nouveau mon petit ?",
+    "Ah fort heureusement que j'ai une bonne mémoire mais une mauvaise vue, pourrais-tu me reposer ta question différement ?",
+    "Bon sang, avec l'âge...as-tu vue mes lunettes dans le coin ? Mince...elles sont autour de mon cou. Peux-tu me reposer la question?"
+    "Peux-tu me préciser mieux le lieux ? "
+]
+
+
+positive_answers = [
+    "Ah tu as bien de la chance que j'ai une mémoire d'éléphant ! Je vais te dire tout ça mon petit, laisse moi voir par où je commence",
+    "ça tombe bien ! Nous nous sommes déplacés avec Grandmy quand nous étions jeune. Je me rappelle certaines choses, laisse moi te le ranconter",
+    "Je me souviens comme si c'était hier...voici d'autres informations pour ta question jeunot",
+    "Quand l'heure de souper arrivera, je ne pourrais pas tout t'expliquer car j'aurai la tête ailleur, écoutes donc cette histoire en guise de réponse"
+    "C'était ton père qui adorait cet endroit, drôle de vie ! Laisse moi te raconter en détails comment c'était selon mes souvenirs bien lointain",
+    "J'ai épousé ta mamie proche de ce lieux, nous y sommes passés en charette juste avant. Laisse  moi  donc te raconter tout ça."
+]
