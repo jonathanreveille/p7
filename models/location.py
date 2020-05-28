34814a5e9a9f1d517d4d7531da1f8d367c3456bc
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2020-05-28 15:54:58 +0200

    module with class to create lat and lng positions

---
+++ models/location.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python3
+# coding : utf-8
+
+""" Class to create positions with latitude and longitude"""
+
+class Location:
+
+    def __init__(self, lat, lng):
+        self.latitude = float(lat)
+        self.longitude = float(lng)
+
+    def __repr__(self):
+        """Method that presents in first position
+        latitude, and second position the longitude of 
+        a place """
+
+        return f"Location : {self.latitude}, {self.longitude}"
+
+if __name__ == "__main__":
+    pass
+
+def main():
+    pass
+
