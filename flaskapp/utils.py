ea35b59970fe3969e437426d7966a6e60530ff68
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2020-06-30 19:51:52 +0200

    module to interact with retrieved data from user

---
+++ flaskapp/utils.py
@@ -0,0 +1,20 @@
+# Nous utiliserons ce fichier pour traiter les 
+# donnés que nous allons récupérer avec le navigateur
+# grâce à JS et ses méthodes.
+from models.parser import Parser
+
+def transform_to_upper(text):
+    print(text)
+    return {
+        "text-original":text,
+        "text-transformed": text.upper()
+    }
+
+
+# 1 créer une méthode pour récupérer le texte de l'user
+# 2 Isoler la phrase texte sur laquelle nous allons travailler dessus
+# 3 utiliser la méthode de Parser (class) pour regex
+# 4 envoyer à Google Maps et récupérer Longitude latitude
+# 5 Envoyer à media Wiki pour récupérer le résumé sur le lieu dit
+# 6 retourner la réponse en f'string' pour que ce soit comme on veut
+# 7 trouver la façon de pouvoir le réinjecter dans le HTML
