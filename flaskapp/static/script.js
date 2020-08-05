8f02dad4ec9ddb0cb812075fe8000c098d9e7dd5
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2020-08-05 17:17:31 +0200

    ajax fonctionne navigateur serveur, serveur navigateur

--- flaskapp/static/script.js
+++ flaskapp/static/script.js
@@ -5,44 +5,54 @@
 let form = document.querySelector("#user-text-form");
 
 // NEW FUNCTION : CREATE ELEMENT AND APPENDCHILD
-function createDiv(text,parent){
+function createDiv(text, parent, id){
 
     let div = document.createElement('div');
-    div.id = 'chatbox';
+    div.id = id;
     div.textContent = text;
     parent.appendChild(div);
+    return div
 }
-
 
     // On crée une fonction qui agira selon la méthode qu'on définit 
     // dans notr cas, nous souhaitons récupérer ce que  l'user à mi
-function postFormData (url, data, headers) {
-    return fetch(url, {
-                method:"POST",
-                body: data,
-                headers : headers,
-                })
-
-        .then(response => response.json()) //fonction anonyme de ES6
-        .catch(error => console.log(error));
+async function postFormData (url, data, headers) {
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            body: data,
+            headers: headers,
+        });
+        return await response.json();
+    }
+    catch (error) {
+        return console.log(error);
+    }
     }
 
-    form.addEventListener("submit", function (event) {
-        event.preventDefault();
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
 
     // Envoyer le contenu du formulaire au serveur
     postFormData("/ajax", document.querySelector('#userText').value, {
         "Content-Type": "plain/text"
-    })
+        })
+        .then(response => {
+            createDiv(response.article, chatbox, "chatbox")
+            createDiv(response.url, chatbox, "chatbox")
+            createDiv(response.location, gmap, "gmap")})
+        })
 
-    // Voir le résultat dans la console avec les outils de navigateur
-    // on veut créer des nouveaux éléments dans le DOM
-    .then(response => {console.log(response); 
-    })
+                // .then(response => {console.log(response)}
 
-    let chatbox = document.querySelector("#chatbox")
-    createDiv("On se calme", chatbox);
-    })
+
+        
+// Voir le résultat dans la console avec les outils de navigateur
+// on veut créer des nouveaux éléments dans le DOM
+
+    // let chatbox = document.querySelector("#chatbox")
+    // createDiv(response, chatbox);
+    // })
 
     // on créer une div qui permet de chatbox lors du SUBMIT
 
